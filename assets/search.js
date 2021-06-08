adedffa49825a464b30198f0e87289f59a564dc3
captainbrosset <captainbrosset@users.noreply.github.com>
2021-06-08 10:06:41 +0000

    deploy: 5c340cb0869c0e84e2a54d1726b03dfdbe9e9171

---
+++ assets/search.js
@@ -0,0 +1,65 @@
+"use strict";
+
+function findMatchingTips(query) {
+    return allTips.filter(tip => {
+        // TODO.
+        return tip.title.toLowerCase().includes(query.toLowerCase());
+    });
+}
+
+let resultPanel = null;
+
+function emptyResults() {
+    if (resultPanel) {
+        resultPanel.innerHTML = '';
+    }
+}
+
+function showMatchingTips(tips, query) {
+    if (!resultPanel) {
+        resultPanel = document.querySelector('.search-results');
+    }
+
+    resultPanel.innerHTML = '';
+
+    for (const { title, url } of tips) {
+        const li = document.createElement('li');
+        li.classList.add('result', 'hover-box', 'tip');
+
+        const a = document.createElement('a');
+        a.href = url;
+
+        const matchStart = title.toLowerCase().indexOf(query.toLowerCase());
+        const matchLength = query.length;
+
+        a.appendChild(document.createTextNode(title.substring(0, matchStart)));
+
+        const highlighted = document.createElement('span');
+        highlighted.classList.add('highlight');
+        highlighted.textContent = title.substring(matchStart, matchStart + matchLength);
+        a.appendChild(highlighted);
+
+        a.appendChild(document.createTextNode(title.substring(matchStart + matchLength)));
+
+        li.appendChild(a);
+
+        debugger;
+
+        resultPanel.appendChild(li);
+    }
+}
+
+const searchField = document.querySelector('#search');
+searchField.addEventListener('input', () => {
+    const q = searchField.value.trim();
+    if (q.length < 2) {
+        emptyResults();
+        return;
+    }
+    const tips = findMatchingTips(q);
+    if (tips.length) {
+        showMatchingTips(tips, q);
+    } else {
+        emptyResults();
+    }
+});
