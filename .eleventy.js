6a4ee3695adb4dacf1592d4751247868c7005390
Patrick Brosset <patrickbrosset@gmail.com>
2021-06-24 22:07:28 +0200

    Added authors

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,43 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { exec } = require("child_process");
+
+function execCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, function (err, stdout, stderr) {
+      if (err != null) {
+        reject(new Error(err));
+      } else if (typeof (stderr) != "string") {
+        reject(new Error(stderr));
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+async function getTipAuthors(tipPath) {
+  const results = await execCommand(`git log --pretty=format:"%an" ${tipPath}`);
+  return [...new Set(results.split('\n'))];
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("CNAME");
 
-  eleventyConfig.addFilter("processBrowserTagName", function(name) {
+  eleventyConfig.addNunjucksAsyncShortcode("authors", async function (path) {
+    const authors = await getTipAuthors(path);
+    return `<ul class="authors">${authors.map(name => `<li>${name}</li>`).join('')}</ul>`;
+  });
+
+  eleventyConfig.addFilter("processBrowserTagName", function (name) {
     return name.split(':')[1];
   });
 
-  eleventyConfig.addFilter("onlyTags", function(tags) {
+  eleventyConfig.addFilter("onlyTags", function (tags) {
     return tags.filter(tag => !tag.startsWith('browser:'));
   });
 
-  eleventyConfig.addFilter("onlyBrowsers", function(browsers) {
+  eleventyConfig.addFilter("onlyBrowsers", function (browsers) {
     return browsers.filter(browser => browser.startsWith('browser:'));
   });
 
