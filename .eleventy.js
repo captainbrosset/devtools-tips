be0b40e877454cb6c2e8134fbf80f54e6e230f08
Patrick Brosset <patrickbrosset@gmail.com>
2021-06-24 22:19:54 +0200

    debugging authors

--- .eleventy.js
+++ .eleventy.js
@@ -2,31 +2,32 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const { exec } = require("child_process");
 
 function execCommand(command) {
-  return new Promise((resolve, reject) => {
-    exec(command, function (err, stdout, stderr) {
-      if (err != null) {
-        reject(new Error(err));
-      } else if (typeof (stderr) != "string") {
-        reject(new Error(stderr));
-      } else {
-        resolve(stdout);
-      }
+    return new Promise((resolve, reject) => {
+        exec(command, function(err, stdout, stderr) {
+            if (err != null) {
+                reject(new Error(err));
+            } else if (typeof(stderr) != "string") {
+                reject(new Error(stderr));
+            } else {
+                resolve(stdout);
+            }
+        });
     });
-  });
 }
 
 async function getTipAuthors(tipPath) {
-  const results = await execCommand(`git log --pretty=format:"%an" ${tipPath}`);
-  return [...new Set(results.split('\n'))];
+    const results = await execCommand(`git log --pretty=format:"%an" ${tipPath}`);
+    console.log('Found authors', results);
+    return [...new Set(results.split('\n'))];
 }
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("CNAME");
+module.exports = function(eleventyConfig) {
+        eleventyConfig.addPassthroughCopy("src/assets");
+        eleventyConfig.addPassthroughCopy("CNAME");
 
-  eleventyConfig.addNunjucksAsyncShortcode("authors", async function (path) {
-    const authors = await getTipAuthors(path);
-    return `<ul class="authors">${authors.map(name => `<li>${name}</li>`).join('')}</ul>`;
+        eleventyConfig.addNunjucksAsyncShortcode("authors", async function(path) {
+                    const authors = await getTipAuthors(path);
+                    return `<ul class="authors">${authors.map(name => `<li>${name}</li>`).join('')}</ul>`;
   });
 
   eleventyConfig.addFilter("processBrowserTagName", function (name) {
