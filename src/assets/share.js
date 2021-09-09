a4a5277ee6b67e5cc61eee642bf3d9c68130094f
Patrick Brosset <patrickbrosset@gmail.com>
2021-09-09 16:15:24 +0200

    Added files when sharing tips

--- src/assets/share.js
+++ src/assets/share.js
@@ -10,13 +10,32 @@ function createTipUI() {
     container.appendChild(shareBtn);
 }
 
+async function getImagesAsFiles(images) {
+    const files = [];
+    for (const image of images) {
+        // Easiest way is to fetch them again, which is fine, it's already in cache.
+        const response = await fetch(image.src);
+        const blob = await response.blob();
+        const name = image.src.substring(image.src.lastIndexOf('/') + 1);
+        files.push(new File([blob], name));
+    }
+    return files;
+}
+
 async function shareTip() {
     const url = document.location.href;
     const title = `DevTools Tip - ${document.title}`;
     const text = document.querySelector('.tip-content').textContent;
+    const data = {title, text, url};
+
+    // Add image files if supported.
+    const images = await getImagesAsFiles([...document.querySelectorAll('.tip-content img')]);
+    if (navigator.canShare && navigator.canShare({ files: images })) {
+        data.files = images;
+    }
 
     try {
-        await navigator.share({title, text, url});
+        await navigator.share(data);
     } catch (e) {
         console.error('Error sharing this tip', e);
     }
