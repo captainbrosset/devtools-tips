a19d2598fb290d2ed3554cbe294f5fa4fcdad8dd
Patrick Brosset <patrickbrosset@gmail.com>
2024-02-28 16:34:09 +0100

    trying to get files

--- .github/scripts/generate-pr-description.js
+++ .github/scripts/generate-pr-description.js
@@ -1,16 +1,17 @@
-const github = require('@actions/github');
+const github = require("@actions/github");
+const github = require("@actions/github");
 
 async function main() {
-  // const octokit = github.getOctokit(process.env.token);
+  const octokit = github.getOctokit(process.env.token);
+  const context = github.context;
 
-  // Retrieve the list of md files that the PR is modifying
-  // and that's being retrieved from the github action.
-  const files = github.context.payload.pull_request.files;
+  const { data: files } = await octokit.pulls.listFiles({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+  });
+
   console.log(files);
-
-  // Retrieve the command line arguments that were passed to the action.
-  const args = process.argv.slice(2);
-  console.log(args);
 }
 
 main();
