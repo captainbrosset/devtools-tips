f70c0a3a17335a3927f843f6f2b3ce3bf7289776
Patrick Brosset <patrickbrosset@gmail.com>
2026-02-03 16:36:50 +0100

    Update service worker version and clean up code
    Updated service worker version and removed unused background sync and notification handling code.

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,9 @@
 self.importScripts('/assets/localforage-1.10.0.min.js');
 
-// UPDATED: 11/16/2023
+// UPDATED: 02/03/2026
 
-const VERSION = 'v12';
+const VERSION = 'v13';
 const CACHE_NAME = `devtools-tips-${VERSION}`;
-
-const PERIODIC_UPDATE_SUPPORTED = ('periodicSync' in registration);
 
 // Static resources to cache initially.
 const INITIAL_CACHED_RESOURCES = [
@@ -56,11 +54,8 @@ self.addEventListener('fetch', event => {
 
     try {
       const fetchResponse = await fetch(event.request);
-      if (!event.request.url.includes('pixel.php') && !event.request.url.includes('browser-sync')) {
-        // Save the new resource in the cache (responses are streams, so we need to clone in order to use it here).
-        cache.put(event.request, fetchResponse.clone());
-      }
-
+      // Save the new resource in the cache (responses are streams, so we need to clone in order to use it here).
+      cache.put(event.request, fetchResponse.clone());
       // And return it.
       return fetchResponse;
     } catch (e) {
@@ -70,88 +65,9 @@ self.addEventListener('fetch', event => {
         return cachedResponse;
       } else if (event.request.mode === 'navigate') {
         // Couldn't find anything in the cache, and this is a request to a page, let's go to the error page.
-        await rememberRequestedTip(event.request.url);
         const errorResponse = await cache.match('/offline/');
         return errorResponse;
       }
     }
   })());
 });
-
-async function rememberRequestedTip(url) {
-  let tips = await localforage.getItem('bg-tips');
-  if (!tips) {
-    tips = [];
-  }
-
-  tips.push(url);
-  await localforage.setItem('bg-tips', tips);
-}
-
-// Listen to background sync events to load requested tips that couldn't be retrieved when offline.
-self.addEventListener('sync', event => {
-  if (event.tag === 'bg-load-tip') {
-    event.waitUntil(backgroundSyncLoadTips());
-  }
-});
-
-// Fetch the requested tips now, and put them in cache.
-async function backgroundSyncLoadTips() {
-  const tips = await localforage.getItem('bg-tips');
-  if (!tips || !tips.length) {
-    return;
-  }
-
-  // Fetch and cache each tip.
-  const cache = await caches.open(CACHE_NAME);
-  await cache.addAll(tips);
-
-  // Re-engage user with a notification.
-  registration.showNotification(`${tips.length} DevTools Tips was/were loaded in the background and is/are ready`, {
-    icon: "/assets/logo-192.png",
-    body: "View the tip",
-    data: tips[0]
-  });
-
-  await localforage.removeItem('bg-tips');
-}
-
-self.addEventListener('notificationclick', event => {
-  // assuming only one type of notification right now
-  event.notification.close();
-  clients.openWindow(event.notification.data);
-});
-
-// Listen the periodic background sync events to update the cached resources.
-self.addEventListener('periodicsync', event => {
-  if (event.tag === 'update-cached-content') {
-    event.waitUntil(updateCachedContent());
-  }
-});
-
-async function updateCachedContent() {
-  const requests = await findCacheEntriesToBeRefreshed();
-  const cache = await caches.open(CACHE_NAME);
-
-  for (const request of requests) {
-    try {
-      // Fetch the new version.
-      const fetchResponse = await fetch(request);
-      // Refresh the cache.
-      await cache.put(request, fetchResponse.clone());
-    } catch (e) {
-      // Fail silently, we'll just keep whatever we already had in the cache.
-    }
-  }
-}
-
-// Find the entries that are already cached and that we want to periodically update.
-// There are some resources we never want to bother with updating.
-// They can always be force-updated by updating the CACHE version.
-async function findCacheEntriesToBeRefreshed() {
-  const cache = await caches.open(CACHE_NAME);
-  const requests = await cache.keys();
-  return requests.filter(request => {
-    return !DONT_UPDATE_RESOURCES.some(pattern => request.url.includes(pattern));
-  });
-}
